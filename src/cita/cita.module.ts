d15a5882a491160812012a0b3b2e08ed0d2e0510
Jorge Mejicanos <jorgemejicanos145@gmail.com>
2025-09-17 22:07:18 -0600

    citas y medicos

--- src/cita/cita.module.ts
+++ src/cita/cita.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CitaService } from './cita.service';
 import { CitaController } from './cita.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [CitaController],
   providers: [CitaService],
 })
--- src/medico/medico.module.ts
+++ src/medico/medico.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MedicoService } from './medico.service';
 import { MedicoController } from './medico.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [MedicoController],
   providers: [MedicoService],
 })
